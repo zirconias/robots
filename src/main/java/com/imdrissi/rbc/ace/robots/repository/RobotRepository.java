9de4e9a803a053cb8c1d236fc22fb07ffefbc359
islam drissi <islam.drissi@gmail.com>
2017-07-08 00:20:27 +0100

    adding repo methods

--- src/main/java/com/imdrissi/rbc/ace/robots/repository/RobotRepository.java
+++ src/main/java/com/imdrissi/rbc/ace/robots/repository/RobotRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface RobotRepository extends JpaRepository<Robot, Long> {
 
   void deleteByProductId(String productId);
@@ -12,5 +14,11 @@ public interface RobotRepository extends JpaRepository<Robot, Long> {
   @Modifying
   @Query("update #{#entityName} e set e.deleted=true where e.productId=?1")
   void softDeleteByProductId(String productId);
+
+  @Query("select e from #{#entityName} e where e.deleted=false")
+  public List<Robot> findAllNotDeleted();
+
+  @Query("select e from #{#entityName} e where e.deleted=true")
+  public List<Robot> findBin();
 
 }

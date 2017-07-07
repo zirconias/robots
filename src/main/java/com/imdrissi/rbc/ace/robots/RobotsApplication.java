1c91ba575804f06bb1bb62bb523326d2b9223314
islam drissi <islam.drissi@gmail.com>
2017-07-07 21:53:50 +0100

    Initial commit

---
+++ src/main/java/com/imdrissi/rbc/ace/robots/RobotsApplication.java
@@ -0,0 +1,12 @@
+package com.imdrissi.rbc.ace.robots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotsApplication.class, args);
+	}
+}

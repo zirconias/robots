5c00428167f4cc6d068a38f0c302366c46d9cfe3
islam drissi <islam.drissi@gmail.com>
2017-07-08 23:32:00 +0100

    rest endpoints done

--- src/main/java/com/imdrissi/rbc/ace/robots/rest/RobotController.java
+++ src/main/java/com/imdrissi/rbc/ace/robots/rest/RobotController.java
@@ -4,14 +4,16 @@ import com.imdrissi.rbc.ace.robots.domain.Robot;
 import com.imdrissi.rbc.ace.robots.service.RobotService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -19,6 +21,8 @@ import java.security.Principal;
 @RestController
 @RequestMapping(value = "/robot", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RobotController {
+
+  private static final Logger log = LoggerFactory.getLogger(RobotController.class);
 
   @Autowired
   RobotService robotService;
@@ -34,6 +38,41 @@ public class RobotController {
   @GetMapping
   ResponseEntity<Iterable<Robot>> robots() {
     return ResponseEntity.ok(robotService.allRobots());
+  }
+
+  @ApiOperation(value = "Get robot by id", notes = "returns a robot by id")
+  @GetMapping("/{robotId}")
+  ResponseEntity<Robot> getRobotByRobotId(@PathVariable String robotId) {
+    Robot r = robotService.getRobotByRobotId(robotId);
+    return new ResponseEntity<Robot>(r, HttpStatus.OK);
+  }
+
+  @ApiOperation(value = "Add new Robot to store", notes = "adds new robot to store")
+  @ApiResponses(value = {@ApiResponse(code = 400, message = "validation errors")})
+  @PostMapping
+  ResponseEntity<Robot> addRobot(@RequestBody Robot robot) {
+    Robot r = robotService.saveRobot(robot);
+    return new ResponseEntity<Robot>(r, HttpStatus.OK);
+  }
+
+  @ApiOperation(value = "Update Robot", notes = "Updates robot with given robotId")
+  @PutMapping(value = "/{robotId}")
+  public ResponseEntity<Robot> updateUser(@PathVariable("robotId") String robotId, @RequestBody Robot robot) {
+    log.info("Updating Robot with robotId {}", robotId);
+
+    Robot currentRobot = robotService.findByRobotId(robotId);
+
+    if (currentRobot == null) {
+      log.error("Unable to update. Robot with robotId {} not found.", robotId);
+      return new ResponseEntity("{\"error\":\"robot with productId" + robotId + "not found!\"}", HttpStatus.NOT_FOUND);
+    }
+
+    currentRobot.setDescription(robot.getDescription());
+    currentRobot.setImageUrl(robot.getImageUrl());
+    currentRobot.setPrice(robot.getPrice());
+
+    robotService.updateRobot(currentRobot);
+    return new ResponseEntity<Robot>(currentRobot, HttpStatus.OK);
   }
 
 
